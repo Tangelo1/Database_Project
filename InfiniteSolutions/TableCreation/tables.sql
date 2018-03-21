b17bfeba658b0aee7db2864935de4ef397f8a8ef
Erkan Uretener <erkan808987@gmail.com>
2018-03-21 11:56:45 -0400

    Added the package,order,location,shippingCostMultipliers

--- InfiniteSolutions/TableCreation/tables.sql
+++ InfiniteSolutions/TableCreation/tables.sql
@@ -42,3 +42,38 @@ CREATE TABLE location (
   name VARCHAR(50),
   type VARCHAR(1)
 );
+//
+CREATE TABLE package (
+  tracking_id INT PRIMARY KEY,
+  weight DOUBLE,
+  type VARCHAR(10),
+  speed VARCHAR(10),
+  value DOUBLE,
+  destination_addr_id INT,
+  source_addr_id INT,
+  isHazard BOOLEAN,
+  isInternational BOOLEAN,
+  FOREIGN KEY (destination_addr_id) REFERENCES address,
+  FOREIGN KEY (source_addr_id) REFERENCES address,
+) ;
+//
+CREATE TABLE order (
+  order_id INT PRIMARY KEY,
+  tracking_id INT,
+  account_id INT,
+  date TIMESTAMP,
+  cost DOUBLE,
+  FOREIGN KEY (tracking_id) REFERENCES package,
+  FOREIGN KEY (account_id) REFERENCES  account
+);
+//
+CREATE TABLE location (
+  location_id INT PRIMARY KEY,
+  name VARCHAR(20),
+  type VARCHAR(10)
+);
+//
+CREATE TABLE shippingCostMultipliers (
+  multiplier VARCHAR(30) PRIMARY KEY,
+  value DOUBLE,
+);
