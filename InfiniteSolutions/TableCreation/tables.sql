c9463fc72d07ccb4175f1f993b850a987eb2e0f3
Tyler Angelo <T.angelo@hotmail.com>
2018-03-21 11:51:51 -0400

    Added tables sql files

---
+++ InfiniteSolutions/TableCreation/tables.sql
@@ -0,0 +1,38 @@
+//State can also be a province or a county
+//Street can also be PO box
+CREATE TABLE address (
+  id INT PRIMARY KEY,
+  street VARCHAR(50),
+  city VARCHAR(50),
+  state VARCHAR(50),
+  zip INT,
+  country VARCHAR(50));
+//
+CREATE TABLE account (
+  id INT PRIMARY KEY,
+  type VARCHAR(1),
+  name VARCHAR(50),
+  phone VARCHAR(15),
+  credit_card_id INT,
+  FOREIGN KEY (credit_card_id) REFERENCES credit_card,
+  billing_address_id INT,
+  FOREIGN KEY (billing_address_id) REFERENCES address);
+//
+CREATE TABLE manifestItem (
+  tracking_id INT,
+  FOREIGN KEY (tracking_id) REFERENCES package);
+//
+CREATE TABLE trackingEvents (
+  tracking_id INT,
+  FOREIGN KEY (tracking_id) REFERENCES package,
+  location_id INT,
+  FOREIGN KEY (location_id) REFERENCES location,
+  date TIMESTAMP,
+  status VARCHAR(20));
+//
+CREATE TABLE credit_card (
+  name VARCHAR(50),
+  number VARCHAR(16) PRIMARY KEY,
+  exp_date DATE,
+  cvv VARCHAR(4)
+);
