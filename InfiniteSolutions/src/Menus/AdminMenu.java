69f25e6526776767f5fc7e0148319381adacb740
sxk2522 <sxk2522@rit.edu>
2018-03-23 16:57:15 -0400

    Implemented stubbed out version of admin menu.

--- InfiniteSolutions/src/Menus/AdminMenu.java
+++ InfiniteSolutions/src/Menus/AdminMenu.java
@@ -8,6 +8,21 @@ public class AdminMenu {
     /** Menu choice to enter the sql console. */
     private static final int SQL_CONSOLE = 1;
 
+    /** Corporate account menu selection */
+    private static final int CORPORATE_ACCOUNT = 2;
+
+    /** Monitor network locations menu selection */
+    private static final int MONITOR_NETWORK = 3;
+
+    /** View package details menu selection. */
+    private static final int PACKAGE_DETAILS = 4;
+
+    /** The order details menu selection */
+    private static final int ORDER_DETAILS = 5;
+
+    /** The bill customers menu selection. */
+    private static final int BILL_CUSTOMERS = 6;
+
     /** Menu option for loging out. */
     private static final int LOG_OUT = 7;
 
@@ -23,7 +38,73 @@ public class AdminMenu {
         do {
             // Make a menu selection
             menuSelection = Input.makeSelectionInRange(SQL_CONSOLE, LOG_OUT);
+
+            switch(menuSelection) {
+                case SQL_CONSOLE:
+                    enterSQLConsole();
+                    break;
+                case CORPORATE_ACCOUNT:
+                    createCorporateAccount();
+                    break;
+                case MONITOR_NETWORK:
+                    viewLocation();
+                    break;
+                case PACKAGE_DETAILS:
+                    viewOrders();
+                    break;
+                case ORDER_DETAILS:
+                    chargeCorporateCustomers();
+                    break;
+                case BILL_CUSTOMERS:
+                    chargeCorporateCustomers();
+                    break;
+                case LOG_OUT:
+                    System.out.println("Goodbye.");
+                    System.exit(0);
+                    break;
+            }
+
         } while (menuSelection != LOG_OUT);
     }
 
+    /**
+     * Delivers user to the SQL console.
+     */
+    private static void enterSQLConsole() {
+        // TODO
+        System.out.println("SQL Console Method Stub");
+    }
+
+    /**
+     * The sub menu procedure for creating corporate accounts.
+     */
+    private static void createCorporateAccount() {
+        // TODO
+        System.out.println("Corporate Account Creation Method Stub");
+    }
+
+    /**
+     * Views locations within the network.
+     */
+    private static void viewLocation() {
+        // TODO
+        System.out.println("View Location Method Stub");
+    }
+
+    /**
+     * Sub menu for viewing orders for customers and date ranges.
+     */
+    private static void viewOrders() {
+        // TODO
+        System.out.println("View Orders Method Stub");
+    }
+
+
+    /**
+     * Sub menu for charging all customers.
+     */
+    private static void chargeCorporateCustomers() {
+        // TODO
+        System.out.println("Bill Corporate Customers Method Stub");
+    }
 }
